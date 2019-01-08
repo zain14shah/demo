a4b5f39f174d486d1b91a3dfea297b2a32fe112f
Zain Shah <zain.shah@arbisoft.com>
2019-01-08 15:26:30 +0500

    added DynamiteClothing Spider

--- DynamiteClothing.py
+++ DynamiteClothing.py
@@ -1,68 +1,101 @@
-import scrapy
+"""This spider is used to scrape products and their details from
+https://www.dynamiteclothing.com/us
+"""
+
+from scrapy import FormRequest
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 from dynamiteclothing.items import Product
+from scrapy.exceptions import DropItem
 
 
 class DynamiteclothingSpider(CrawlSpider):
     name = 'DynamiteClothing'
     allowed_domains = ['www.dynamiteclothing.com']
+    start_urls = [
+        'https://www.dynamiteclothing.com/us/profile/login.jsp?h=true']
 
+    ids_seen = set()
+    homepage_urls_css = ['.subcategory.subCatLink', '#catPageNext']
+    product_urls_css = '[title="View Product Details"]'
     rules = (
-        Rule(LinkExtractor(restrict_css=[
-            '.subcategory.subCatLink', '#catPageNext'])),
+        Rule(LinkExtractor(restrict_css=homepage_urls_css)),
         Rule(LinkExtractor(
-            restrict_css='[title="View Product Details"]'),
+            restrict_css=product_urls_css),
             callback='parse_product')
     )
 
-    def start_requests(self):
-        yield scrapy.Request(
-            'https://www.dynamiteclothing.com/us/profile/login.jsp?h=true',
-            callback=self.generate_login_request)
-
-    def generate_login_request(self, response):
-        return scrapy.FormRequest.from_response(
+    def parse_start_url(self, response):
+        return FormRequest.from_response(
             response,
             formid='loginForm',
             formdata={
                 'loginEmail': 'zain@gmail.com',
                 '/atg/userprofiling/ProfileFormHandler.value.password': 'ABc123'},
-            callback=self.after_login
+            callback=self.parse_check_login
         )
 
-    def after_login(self, response):
+    def parse_check_login(self, response):
         if response.css('[href*="profile.jsp"]::attr(href)').extract_first():
-            yield scrapy.Request('https://www.dynamiteclothing.com/us?li=true',
-                                 callback=self.parse)
+            yield response.follow('us?li=true',
+                                  callback=self.parse)
         else:
             self.logger.error('Login failed')
             return
 
-    def parse_product(self, response):
-        product = Product()
-        product['name'] = response.css(
-            '.prodName::text').extract_first().strip()
-        product['prod_id'] = response.css(
-            '#productId::attr(value)').extract_first()
-        product['price'] = response.css(
-            '.prodPricePDP::text, .salePrice::text').re_first('\S+')
-        product['sale_price'] = response.css(
-            '.withSale::text').extract_first('N/A').strip()
-        product['colors'] = response.css(
-            '.swatchColor::text').extract_first().strip()
-        product['sizes'] = response.css('#productSizes span::text').extract()
-        product['description'] = response.css(
+    def product_name(self, response):
+        return response.css('.prodName::text').extract_first().strip()
+
+    def product_id(self, response):
+        id = response.css('#productId::attr(value)').extract_first()
+        if id in self.ids_seen:
+            raise DropItem(f'Duplicate product found: {id}')
+        else:
+            self.ids_seen.add(id)
+            return id
+
+    def product_price(self, response):
+        return response.css(
+            '.prodPricePDP::text, .salePrice::text'
+        ).re_first('\S+')
+
+    def product_sale_price(self, response):
+        return response.css('.withSale::text').extract_first('N/A').strip()
+
+    def product_colors(self, response):
+        return response.css('.swatchColor::text').extract_first().strip()
+
+    def product_sizes(self, response):
+        return response.css('#productSizes span::text').extract()
+
+    def product_description(self, response):
+        return response.css(
             '#descTab0Content p::text, #descTabDescriptionContent p::text'
         ).extract_first().replace('\u00a0', '')
+
+    def product_details(self, response):
         details_with_problems = response.css(
             '#descTabDetailsContent li::text, #descTab0Content li::text, #descTabDescriptionContent li::text'
         ).extract()
-        details = []
-        for each_detail in details_with_problems:
-            details.append(each_detail.replace('\u00a0', ' ').strip())
-        product['details'] = details
-        product['img_URLs'] = response.css('a::attr(data-zoom)').extract()
+        return [
+            each_detail.replace('\u00a0', ' ').strip()
+            for each_detail in details_with_problems
+            ]
+
+    def product_img_urls(self, response):
+        return response.css('a::attr(data-zoom)').extract()
+
+    def parse_product(self, response):
+        product = Product()
+        product['name'] = self.product_name(response)
+        product['id'] = self.product_id(response)
+        product['price'] = self.product_price(response)
+        product['sale_price'] = self.product_sale_price(response)
+        product['colors'] = self.product_colors(response)
+        product['sizes'] = self.product_sizes(response)
+        product['description'] = self.product_description(response)
+        product['details'] = self.product_details(response)
+        product['img_urls'] = self.product_img_urls(response)
 
         return product
--- items.py
+++ items.py
@@ -1,20 +1,13 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy.item import Item, Field
 
 
-class Product(scrapy.Item):
-    name = scrapy.Field()
-    prod_id = scrapy.Field()
-    price = scrapy.Field()
-    sale_price = scrapy.Field()
-    colors = scrapy.Field()
-    sizes = scrapy.Field()
-    description = scrapy.Field()
-    details = scrapy.Field()
-    img_URLs = scrapy.Field()
+class Product(Item):
+    name = Field()
+    id = Field()
+    price = Field()
+    sale_price = Field()
+    colors = Field()
+    sizes = Field()
+    description = Field()
+    details = Field()
+    img_urls = Field()
