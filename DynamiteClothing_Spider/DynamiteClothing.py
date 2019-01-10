dbed9864be0c2ed2e912674be98a9422e495fa0d
Zain Shah <zain.shah@arbisoft.com>
2019-01-10 17:02:10 +0500

    Improved files

--- DynamiteClothing_Spider/DynamiteClothing.py
+++ DynamiteClothing_Spider/DynamiteClothing.py
@@ -21,9 +21,7 @@ class DynamiteclothingSpider(CrawlSpider):
     product_urls_css = '[title="View Product Details"]'
     rules = (
         Rule(LinkExtractor(restrict_css=homepage_urls_css)),
-        Rule(LinkExtractor(
-            restrict_css=product_urls_css),
-            callback='parse_product')
+        Rule(LinkExtractor(restrict_css=product_urls_css), callback='parse_product')
     )
 
     def parse_start_url(self, response):
@@ -38,16 +36,15 @@ class DynamiteclothingSpider(CrawlSpider):
 
     def parse_check_login(self, response):
         if response.css('[href*="profile.jsp"]::attr(href)').extract_first():
-            yield response.follow('us?li=true',
-                                  callback=self.parse)
+            yield response.follow('us?li=true', callback=self.parse)
         else:
             self.logger.error('Login failed')
             return
 
-    def product_name(self, response):
+    def parse_product_name(self, response):
         return response.css('.prodName::text').extract_first().strip()
 
-    def product_id(self, response):
+    def parse_product_id(self, response):
         id = response.css('#productId::attr(value)').extract_first()
         if id in self.ids_seen:
             raise DropItem(f'Duplicate product found: {id}')
@@ -55,47 +52,45 @@ class DynamiteclothingSpider(CrawlSpider):
             self.ids_seen.add(id)
             return id
 
-    def product_price(self, response):
-        return response.css(
-            '.prodPricePDP::text, .salePrice::text'
-        ).re_first('\S+')
+    def parse_product_price(self, response):
+        return response.css('.prodPricePDP::text, .salePrice::text').re_first('\S+')
 
-    def product_sale_price(self, response):
+    def parse_product_sale_price(self, response):
         return response.css('.withSale::text').extract_first('N/A').strip()
 
-    def product_colors(self, response):
+    def parse_product_colors(self, response):
         return response.css('.swatchColor::text').extract_first().strip()
 
-    def product_sizes(self, response):
+    def parse_product_sizes(self, response):
         return response.css('#productSizes span::text').extract()
 
-    def product_description(self, response):
+    def parse_product_description(self, response):
         return response.css(
             '#descTab0Content p::text, #descTabDescriptionContent p::text'
         ).extract_first().replace('\u00a0', '')
 
-    def product_details(self, response):
+    def parse_product_details(self, response):
         details_with_problems = response.css(
             '#descTabDetailsContent li::text, #descTab0Content li::text, #descTabDescriptionContent li::text'
         ).extract()
         return [
             each_detail.replace('\u00a0', ' ').strip()
             for each_detail in details_with_problems
-            ]
+        ]
 
-    def product_img_urls(self, response):
+    def parse_product_img_urls(self, response):
         return response.css('a::attr(data-zoom)').extract()
 
     def parse_product(self, response):
         product = Product()
-        product['name'] = self.product_name(response)
-        product['id'] = self.product_id(response)
-        product['price'] = self.product_price(response)
-        product['sale_price'] = self.product_sale_price(response)
-        product['colors'] = self.product_colors(response)
-        product['sizes'] = self.product_sizes(response)
-        product['description'] = self.product_description(response)
-        product['details'] = self.product_details(response)
-        product['img_urls'] = self.product_img_urls(response)
+        product['name'] = self.parse_product_name(response)
+        product['id'] = self.parse_product_id(response)
+        product['price'] = self.parse_product_price(response)
+        product['sale_price'] = self.parse_product_sale_price(response)
+        product['colors'] = self.parse_product_colors(response)
+        product['sizes'] = self.parse_product_sizes(response)
+        product['description'] = self.parse_product_description(response)
+        product['details'] = self.parse_product_details(response)
+        product['img_urls'] = self.parse_product_img_urls(response)
 
         return product
